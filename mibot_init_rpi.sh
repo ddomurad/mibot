3380078d9a7877209216dca11ce299303cae3062
Daniel Domurad <ddomurad@gmail.com>
2016-03-03 09:57:02 +0000

    building updated

--- mibot_init_rpi.sh
+++ mibot_init_rpi.sh
@@ -2,23 +2,27 @@ echo "Initializing environment ... "
 
 cd ./software/
 
-mkdir ./mibotAccess/build
-mkdir ./mibotDriveStrategy/build
 mkdir ./mibotGPIOInterface/build
-mkdir ./mibotInfoStrategy/build
-mkdir ./mibotServer/build
-mkdir ./mibotStatusStrategy/build
+mkdir ./mibotSensors/build
 mkdir ./mibotUtils/build 
 mkdir ./ServerLauncher/build
+mkdir ./mibotGPS/build
+mkdir ./mibotServer/build
+mkdir ./mibotStatusStrategy/build
+mkdir ./mibotVideoStrategy/build
+mkdir ./mibotDriveStrategy/build
+mkdir ./mibotInfoStrategy/build
+mkdir ./mibotSettingsClient/build
+mkdir ./mibotSystemExecute/build
 
-mkdir /usr/local/etc/mi_bot
-mkdir /usr/local/lib/mi_bot
+sudo mkdir /usr/local/etc/mi_bot
+sudo mkdir /usr/local/lib/mi_bot
 
-chmod 777 /usr/local/etc/mi_bot
-chmod 777 /usr/local/lib/mi_bot
+sudo chmod 777 /usr/local/etc/mi_bot
+sudo chmod 777 /usr/local/lib/mi_bot
 
-chown mibot /usr/local/etc/mi_bot/
-chown mibot /usr/local/lib/mi_bot/
+sudo chown mibot /usr/local/etc/mi_bot/
+sudo chown mibot /usr/local/lib/mi_bot/
 
 echo "export MIBOT_CFG=ENABLE_GPIO" >> ~/.bash_profile
 
